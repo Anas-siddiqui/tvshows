fbafb786e4c5880e81181a218242538962576453
Anas-siddiqui <anas_siddiqui@live.com>
2017-01-03 02:41:12 +0500

    added all files

--- app.js
+++ app.js
@@ -176,10 +176,12 @@ app.post('/skill',  function(req, res) {
                               
                                     var temp_time=body[a].airstamp.split("T");
                                     var final_time="";
+                                  
+                                  temp_time=temp_time[1].split(":");
+                             
                                     
-                                    temp_time=temp_time[1].split(":");
-                                    
-                                   final_time=temp_time[0]+":"+temp_time[1];
+                                final_time=temp_time[0]+":"+temp_time[1];
+                                    final_time=tConvert(final_time);
                                    
                                     
                         
@@ -297,7 +299,17 @@ app.use(function(err, req, res, next) {
   });
 });
 
+function tConvert (time) {
+  // Check correct time format and split into components
+  time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
 
+  if (time.length > 1) { // If time format correct
+    time = time.slice (1);  // Remove full string match value
+    time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+    time[0] = +time[0] % 12 || 12; // Adjust hours
+  }
+  return time.join (''); // return adjusted time or original string
+}
 
 
 module.exports = app;
