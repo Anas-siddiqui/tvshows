23f014a3c48ba55dbe1557bd1a5ebb62a9ab64cd
Anas-siddiqui <anas_siddiqui@live.com>
2017-01-09 21:37:49 +0500

    added all files

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ function requestVerifier(req, res, next) {
   
 // catch 404 and forward to error handler
 
-app.post('/skill',  function(req, res) {
+app.post('/skill',requestVerifier,  function(req, res) {
  
    var temp;
     if (req.body.request.type === 'LaunchRequest') { 
